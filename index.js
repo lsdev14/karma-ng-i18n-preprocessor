4c5fa2449c2aebdbfb651fbcf1047551fd9d945f
kmccanless <kmccanless@commandalkon.com>
2015-01-27 08:33:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var i18n = require('i18n');
+
+var createi18nPreprocessor = function(logger, basePath, config) {
+    config = typeof config === 'object' ? config : {};
+    var localize;
+    if(typeof config.i18n === 'object') {
+        localize = true;
+        i18n.configure(config.i18n);
+    }
+
+    var log = logger.create('preprocessor.i18n');
+
+    return function(content, file, done) {
+        var processed;
+        if(localize) {
+            try {
+                var re = /#{__\("(\S\w*\S)"\)}/g
+                while ((myArray = re.exec(content)) !== null)
+                {
+                    var localized = i18n.__(myArray[1])
+                    content = content.replace(myArray[0],localized);
+                }
+            } catch (e) {
+                log.error('%s\n  at %s', e.message);
+            }
+        }
+        // console.log(content);
+        done(null, content);
+    };
+};
+
+createi18nPreprocessor.$inject = ['logger', 'config.basePath', 'config.ngi18nPreprocessor'];
+
+//module.exports = createi18nPreprocessor;
+module.exports = {
+    'preprocessor:ng-i18n': ['factory', createi18nPreprocessor]
+};
